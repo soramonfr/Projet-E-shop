ba74fff3999a7ee83064ad7b7c13567ed41a794b
sorayacruchet <soraya.cruchet@gmail.com>
2020-11-20 21:34:01 +0100

    incrémentation des quantités

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,6 @@ button.onclick = function (e) {
 // Création tableau d'articles (Panier)
 
 let cart = [];
-
 let article = document.getElementById('article1');
 // article.dataset.ref
 // article.dataset.name
@@ -21,16 +20,18 @@ let article = document.getElementById('article1');
 
 // Au clic sur panier, déclencher une fonction
 article.onclick = function (evt) {
-    console.log(this.dataset.ref);
-
-    // TODO Gérer le cas ou un article existerait déjà 
     // ajoute un obj article au tableau "panier" {}
-    cart.push({
-        ref: this.dataset.ref,
-        name: this.dataset.name,
-        price: this.dataset.price,
-        quantity: 1
-    });
+    let index = cart.findIndex((searchArticle) => searchArticle.ref === this.dataset.ref);
+    if (index !== -1) {
+        cart[index].quantity += 1;
+    } else {
+        cart.push({
+            ref: this.dataset.ref,
+            name: this.dataset.name,
+            price: this.dataset.price,
+            quantity: 1
+        });
+    }
 
     // On ne fait rien d'autre (prévenir refresh sur la page)
     evt.preventDefault();
@@ -39,23 +40,20 @@ article.onclick = function (evt) {
 let displayCartBtn = document.getElementById("displayCart");
 let cartTableElt = document.getElementById("cartTable");
 function displayCart() {
-    /*
-    <tr>
-        <td>NomArticle</td><td>Ref</td><td>Prix</td><td>Qte</td><td>btn</td>
-    </tr>
-    */
+    // Suppression des lignes existantes dans le tableau pour éviter que les lignes soient dupliquées lorsque l'on reclique sur le btn "afficher panier"
+    cartTableElt.tBodies[0].innerHTML = "";
+    // Génération des lignes dans le tableau
     cart.forEach(art => {
         let trElt = document.createElement("tr");
         trElt.innerHTML = "<td><h5 class=\"title text-truncate\">" + art.name + "</h5></td>\
         <td>" + art.ref + "</td>\
-        <td class=\"price\">"+ art.price +"</td>\
-        <td><input type=\"number\" value=\""+ art.quantity +"\"></td>\
-        <td class=\"total text-center\">"+ Number.parseFloat(art.price) * Number.parseInt(art.quantity) +"</td>\
+        <td class=\"price\">"+ art.price + "</td>\
+        <td><input type=\"number\" value=\""+ art.quantity + "\"></td>\
+        <td class=\"total text-center\">"+ Number.parseFloat(art.price) * Number.parseInt(art.quantity) + "</td>\
         <td><button type=\"button\" class=\"btn btn-danger deleteArticle\">Supprimer cet article</button></td>";
         cartTableElt.tBodies[0].appendChild(trElt);
-   }) 
-   console.log(cartTableElt.tBodies);
+    })
+    console.log(cartTableElt.tBodies);
 
 }
 displayCartBtn.addEventListener("click", displayCart);
-
